704a843b84c894a103be737c3871f0ae48e222bf
emdickim <kimlazaropardo@gmail.com>
2025-09-25 18:56:34 +0200

    Activitat acabada

--- 01_Rot13/Rot13.java
+++ 01_Rot13/Rot13.java
@@ -1,25 +1,76 @@
 /* */
 public class Rot13 {
-    char[] AbcMinisculas ="abcdefghijklmnopqrstuvwxyzàèéìíïòóùúüñç".toCharArray();
+    private static final char[] AbcMinisculas ="abcdefghijklmnopqrstuvwxyzàèéìíïòóùúüñç".toCharArray();
 
-    char[] AbcMayusculas ="ABCDEFGHIJKLMNOPQRSTUVWXYZÀÈÉÌÍÏÒÓÙÚÜÑÇ".toCharArray();
+    private static final char[] AbcMayusculas ="ABCDEFGHIJKLMNOPQRSTUVWXYZÀÈÉÌÍÏÒÓÙÚÜÑÇ".toCharArray();
     public static String xifraRot13 (String text) {
-        String xd = "";
-        for (int i = 0; i < AbcMinisculas.length; i++) {
-            
-            
-            //if (Character.isUpperCase(text.charAt(i))) {
-
-            //}
+        String textXifrat = "";
+        int posicio = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < AbcMinisculas.length; j++) {
+                if (Character.isLowerCase(text.charAt(i))) {
+                    if (text.charAt(i) == AbcMinisculas[j]) {
+                        if (j+13 < AbcMinisculas.length) {
+                            textXifrat += AbcMinisculas[j+13] + "";   
+                        } else {
+                        textXifrat += AbcMinisculas[13-(AbcMinisculas.length % j )] + "";
+                        }
+                    }
+                } else if (Character.isUpperCase(text.charAt(i))) {
+                    if (text.charAt(i) == AbcMayusculas[j]) {
+                            if (j+13 < AbcMayusculas.length) {
+                                textXifrat += AbcMayusculas[j + 13] + "";
+                                
+                            } else {
+                            textXifrat += AbcMayusculas[13-(AbcMayusculas.length % j )] + "";
+                            }
+                        }
+                } else {
+                    textXifrat += text.charAt(i);
+                    break;
+                }
+                
+            }
         }
-        return xd;
+        return textXifrat;
+    }
+    public static String desXifraRot13 (String text) {
+
+        String textDesxifrat = "";
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < AbcMinisculas.length; j++) {
+                if (Character.isLowerCase(text.charAt(i))) {
+                    if (text.charAt(i) == AbcMinisculas[j]) {
+                        if (j - 13 >= 0) {
+                            textDesxifrat += AbcMinisculas[j - 13] + "";
+                        }  else {
+                            textDesxifrat += AbcMinisculas[AbcMinisculas.length - (13 % j)] + "";
+                        }
+                    }
+                } else if (Character.isUpperCase(text.charAt(i))) {
+                    if (text.charAt(i) == AbcMayusculas[j]) {
+                            if (j - 13 >= 0) {
+                                textDesxifrat += AbcMayusculas[j-13] + "";
+                                
+                            } else {
+                            textDesxifrat += AbcMayusculas[AbcMayusculas.length - (13 % j)] + "";
+                            }
+                        }
+                } else {
+                    textDesxifrat += text.charAt(i);
+                    break;
+                }
+            }
+        }
+        
+        return textDesxifrat;
     }
     public static void main(String[] args) {
-        System.out.println("Escriu el text que s'ha de xifrar");
-        String text = "Mr. calçot";
+        System.out.println("Ara xifrarem aquest text: RmrcaA.lçotó");
+        String text = "RmrcaA.lçotó";
         System.out.println(xifraRot13(text));
+        text = xifraRot13(text);
+        System.out.println("Ara el desxifrarem");
+        System.out.println(desXifraRot13(text));
     }
-
-
-    //String[] text = {ABC,XYZ,Hola, Mr. calçot,Perdó, per tu què és?}; 
 }
